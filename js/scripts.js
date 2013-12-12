8a16da0fe1b7cdd21693f4ddb63e0ea4ab00c25b
Nitin Pasumarthy <nithanaroy@gmail.com>
2013-12-12 23:52:32 +0530

    Cleaned up. Changed nav bar names. Next pushing to Azure

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,8 @@ $(function() {
 	  autoStart: true
 	});
 	
-	 $('#homeAffix').affix({
-    offset: {
-      top: 100
-    , bottom: function () {
-        return (this.bottom = $('.bs-footer').outerHeight(true))
-      }
-    }
-  })
+	$(".left-nav button.btn").click(function() {
+		$(this).children("span").toggleClass("glyphicon-backward glyphicon-forward");
+		$(this).siblings().toggle();
+	});
 });
