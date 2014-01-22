8c71c60ca7a75ff61110b24df596ec6e7b2cec18
Nitin Pasumarthy <nithanaroy@gmail.com>
2014-01-22 17:04:28 +0530

    Did cleanup - removed unnecessary JS and CSS includes

---
+++ js/getInvolved.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('.bxslider').bxSlider({
+	  captions: true,
+	  autoControls: true,
+	  controls: false,
+	  pause: 2000,
+	  autoStart: true
+	});
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 $(function() {
-	$('.bxslider').bxSlider({
-	  captions: true,
-	  autoControls: true,
-	  controls: false,
-	  pause: 2000,
-	  autoStart: true
-	});
-	
 	$(".left-nav button.btn").click(function() {
 		$(this).children("span").toggleClass("glyphicon-backward glyphicon-forward");
 		$(this).siblings().toggle();
